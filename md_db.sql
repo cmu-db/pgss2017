76724f8fda8e580475b0f1c4b432624ade063035
fishdev <ashwin.srinivasan55@gmail.com>
2017-07-20 13:49:51 -0400

    Fixed PostgreSQL syntax errors, added rawcases table

--- md_db.sql
+++ md_db.sql
@@ -1,81 +1,93 @@
-CREATE TABLE cases (
+-- Temporary table to store HTML for case detail pages
+CREATE TABLE rawcases(
 	case_id VARCHAR(24) PRIMARY KEY NOT NULL,
-	title VARCHAR(128) NOT NULL,
-	court_system VARCHAR(MAX) NOT NULL,
-	case_type VARCHAR(MAX) NOT NULL,
-	filing_date DATE NOT NULL,
-	status VARCHAR(14) NOT NULL,
-	disposition VARCHAR(MAX),
-	disposition_date DATE,
-	violation_county VARCHAR(MAX),
-	violation_date DATE,
+	html TEXT NOT NULL
 );
 
-CREATE TABLE parties (
-	case_id VARCHAR(24) FOREIGN KEY REFERENCES cases(case_id) NOT NULL,
-	id PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(MAX),
-	type VARCHAR(MAX),
-	bus_org_name VARCHAR(MAX),
-	agency_name VARCHAR(MAX),
-	race VARCHAR(MAX),
-	sex ENUM('M','F'),
+-- Actual tables after parsing
+CREATE TABLE cases(
+	case_id VARCHAR(24) PRIMARY KEY NOT NULL,
+	title VARCHAR(128) NOT NULL,
+	court_system VARCHAR NOT NULL,
+	case_type VARCHAR NOT NULL,
+	filing_date DATE NOT NULL,
+	status VARCHAR(14) NOT NULL,
+	disposition VARCHAR,
+	disposition_date DATE,
+	violation_county VARCHAR,
+	violation_date DATE
+);
+
+CREATE TYPE sex AS ENUM(
+	'M',
+	'F'
+);
+
+CREATE TABLE parties(
+	case_id VARCHAR(24) REFERENCES cases NOT NULL,
+	id BIGSERIAL PRIMARY KEY,
+	name VARCHAR,
+	type VARCHAR,
+	bus_org_name VARCHAR,
+	agency_name VARCHAR,
+	race VARCHAR,
+	sex sex,
 	height INT CHECK(height BETWEEN 0 AND 108),
 	weight INT CHECK(weight BETWEEN 0 AND 1000),
 	dob DATE,
-	address VARCHAR(MAX),
-	city VARCHAR(MAX),
+	address VARCHAR,
+	city VARCHAR,
 	state VARCHAR(2),
 	zip VARCHAR(5)
 );
 
-CREATE TABLE attorneys (
-	party_id INT FOREIGN KEY REFERENCES parties(id) NOT NULL,
-	name VARCHAR(MAX),
+CREATE TABLE attorneys(
+	party_id INT REFERENCES parties NOT NULL,
+	name VARCHAR,
 	appearance_date DATE,
 	removal_date DATE,
-	practice_name VARCHAR(MAX),
-	address VARCHAR(MAX),
-	city VARCHAR(MAX),
+	practice_name VARCHAR,
+	address VARCHAR,
+	city VARCHAR,
 	state VARCHAR(2),
 	zip VARCHAR(5)
 );
 
-CREATE TABLE events (
-	case_id VARCHAR(24) FOREIGN KEY NOT NULL,
-	type VARCHAR(MAX),
+CREATE TABLE events(
+	case_id VARCHAR(24) REFERENCES cases NOT NULL,
+	type VARCHAR,
 	date DATE,
 	time VARCHAR(8),
-	result VARCHAR(MAX),
-	result_date DATE,
+	result VARCHAR,
+	result_date DATE
 );
 
 CREATE TABLE charges(
-	case_id VARCHAR(24) FOREIGN KEY REFERENCES cases(case_id) NOT NULL,
-	statute_code VARCHAR(MAX) NOT NULL,
-	charge_description VARCHAR(MAX) NOT NULL,
+	case_id VARCHAR(24) REFERENCES cases NOT NULL,
+	statute_code VARCHAR NOT NULL,
+	charge_description VARCHAR NOT NULL,
 	offense_date_from DATE,
 	offense_date_to DATE,
 	class VARCHAR(1),
-	amended_date VARCHAR(MAX),
-	cjis_code VARCHAR(MAX),
+	amended_date VARCHAR,
+	cjis_code VARCHAR,
 	probable_cause BOOLEAN,
 	victim_age INTEGER CHECK(victim_age BETWEEN 0 AND 150),
 	speed_limit INTEGER CHECK(speed_limit BETWEEN 0 AND 100),
 	recorded_speed INTEGER CHECK(recorded_speed >= 0),
-	location_stopped VARCHAR(MAX),
+	location_stopped VARCHAR,
 	accident_contribution BOOLEAN,
 	injuries INTEGER,
 	property_damage BOOLEAN,
 	seatbelts_used BOOLEAN,
 	mandatory_court_appearance BOOLEAN,
-	vehicle_tag VARCHAR(MAX),
+	vehicle_tag VARCHAR,
 	vehicle_state VARCHAR(2),
-	plea VARCHAR(MAX),
+	plea VARCHAR,
 	plea_date DATE,
-	disposition VARCHAR(MAX),
+	disposition VARCHAR,
 	disposition_date DATE,
-	jail_extreme_punishment VARCHAR(MAX),
+	jail_extreme_punishment VARCHAR,
 	jail_term INTERVAL,
 	jail_suspended_term INTERVAL,
 	jail_unsuspended_term INTERVAL,
@@ -85,34 +97,34 @@ CREATE TABLE charges(
 	fine_amt MONEY,
 	fine_suspended_amt MONEY,
 	fine_restitution_amt MONEY,
-	fine_due DATETIME,
-	fine_first_pmt_due DATETIME,
+	fine_due TIMESTAMP,
+	fine_first_pmt_due TIMESTAMP,
 	cws_hours INTEGER,
-	cws_deadline DATETIME,
-	cws_location VARCHAR(MAX),
-	cws_date DATETIME
+	cws_deadline TIMESTAMP,
+	cws_location VARCHAR,
+	cws_date TIMESTAMP
 );
 
 CREATE TABLE documents(
-	case_id VARCHAR(24) FOREIGN KEY REFERENCES cases(case_id) NOT NULL,
-	name VARCHAR(MAX) NOT NULL,
+	case_id VARCHAR(24) REFERENCES cases NOT NULL,
+	name VARCHAR NOT NULL,
 	filing_date DATE
 );
 
 CREATE TABLE judgements(
-	case_id VARCHAR(24) FOREIGN KEY REFERENCES cases(case_id) NOT NULL,
-	against VARCHAR(MAX),
-	in_favor_of VARCHAR(MAX),
-	type VARCHAR(MAX),
+	case_id VARCHAR(24) REFERENCES cases NOT NULL,
+	against VARCHAR,
+	in_favor_of VARCHAR,
+	type VARCHAR,
 	date DATE,
 	interest MONEY,
 	amt MONEY
 );
 
 CREATE TABLE complaints(
-	case_id VARCHAR(24) FOREIGN KEY REFERENCES cases(case_id) NOT NULL,
-	type VARCHAR(MAX) NOT NULL,
-	status VARCHAR(MAX) NOT NULL,
+	case_id VARCHAR(24) REFERENCES cases NOT NULL,
+	type VARCHAR NOT NULL,
+	status VARCHAR NOT NULL,
 	status_date DATE,
 	filing_date DATE,
 	amt MONEY
