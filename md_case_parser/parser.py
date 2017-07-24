4e20c3185570cf1b0a1b9da0dd14006ff7f9d20c
fishdev <ashwin.srinivasan55@gmail.com>
2017-07-23 23:06:54 -0400

    More parser refactoring (it works now, mostly)

--- md_case_parser/parser.py
+++ md_case_parser/parser.py
@@ -4,46 +4,47 @@ def parseCase(html):
 	# Import into BS
 	soup = BeautifulSoup(html, 'html.parser')
 
-	# Extract all headers, spans, and breaks
-	rows = soup.find_all(['span', 'h5', 'h6', 'hr'])
+	# Temporary KVP store
 	data = {}
-	outdata = {}
+	# Full data list
 	output = []
-	for row in rows:
-		if row.has_attr('class'):
-			classes = row['class']
-			if 'AltBodyWindowDcCivil' not in classes:
-				# Get attribute name
-				if 'FirstColumnPrompt' in classes or 'Prompt' in classes:
-					headerval = row.get_text()
-				# Get corresponding value
-				elif 'Value' in classes:
-					dataval = row.get_text()
-					if headerval:
-						data[headerval] = dataval
-				else:
-					if 'InfoChargeStatement' not in classes:
-						# TODO: clean this up
-						checkH5 = re.findall("h5", str(row))
-						checkH6 = re.findall("h6", str(row))
-						checkHR = re.findall("hr", str(row))
-						checki = re.findall("i", str(row))
-						if len(checkH5) > 0 or len(checkH6) > 0	or len(checki) > 0:
-							if len(outdata) > 0:
-								output.append(outdata)
-								outdata = {}
-							tablehead = row.get_text().strip().replace('/', '-')
-							tablehead = re.sub('\s+', ' ', tablehead)
-							if len(tablehead) > 0:
-								output.append(tablehead)
-						if len(checkHR) > 0:
-							if len(outdata) > 0:
-								output.append(outdata)
-								outdata = {}
-							else:
-								continue
 
-	return data
+	# Get KVPs, headers, and separators
+	rows = soup.find_all(['span', 'h5', 'h6', 'hr'])
+	# Iterate thru page rows
+	for row in rows:
+		# Save class list
+		classes = row.attrs.get('class', [])
+
+		if 'AltBodyWindowDcCivil' not in classes:
+			# Field names
+			if 'FirstColumnPrompt' in classes or 'Prompt' in classes:
+				headerval = row.get_text().strip()
+			# Values
+			elif 'Value' in classes:
+				dataval = row.get_text().strip()
+				if headerval:
+					data[headerval] =  dataval
+			# Headers and separators
+			else:
+				if 'InfoChargeStatement' not in classes:
+					if row.name in {'hr', 'h5', 'h6', 'i'}:
+						# Append the KVPs and reset the temporary dict
+						if data:
+							output.append(data)
+							data = {}
+						# Add the header to the data list
+						if row.name != 'hr':
+							header = row.get_text().strip()
+							if header:
+								output.append(header)
+
+	# Append any remaining KVPs
+	if data:
+		output.append(data)
+		data = {}
+
+	return output
 
 # This is only for testing
 if __name__ == '__main__': print(parseCase(open('test.html', 'r').read()))
