686f998289e863ea9bdde071d82683696f621346
Deepayan Patra <deepayan.patra@gmail.com>
2017-07-31 14:44:11 -0400

    CSV creator made

--- md_learning/makecsv.py
+++ md_learning/makecsv.py
@@ -9,17 +9,23 @@ def main():
     args = sys.argv[1:]
     try:
         conn = psycopg2.connect(host=args[0], database=args[1], user=args[2], password=args[3])
+        print('connected')
     except:
         print('Unable to connect to PostgreSQL')
     cur = conn.cursor()
 
-    getquery = '''SELECT cases.disposition, parties.race, parties.sex, parties.zip, charges.injuries, charges.property_damage
+    getquery = '''SELECT LOWER(charges.disposition), LOWER(parties.race), UPPER(parties.sex), parties.zip, charges.injuries, charges.property_damage
                   FROM cases
                   JOIN parties ON cases.case_id = parties.case_id
                   JOIN charges ON cases.case_id = charges.case_id
                   WHERE NULLIF(cases.disposition, '') IS NOT NULL'''
-
+    print('query complete')
     dataoutput = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(getquery)
-
+    print(dataoutput)
     with open('datafile.csv', 'w') as f:
             cur.copy_expert(dataoutput, f)
+
+    print()
+
+
+if __name__ == '__main__': main()
